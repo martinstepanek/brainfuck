03eaf8ba1048b1815eec8affedf4968319968046
Martin Štěpánek <mstepanek015@gmail.com>
2018-11-01 12:20:33 +0100

    Add Chars

---
+++ BrainfuckInterpreter/BrainfuckInterpreter/Chars.cs
@@ -0,0 +1,14 @@
+namespace BrainfuckInterpreter
+{
+    public class Chars
+    {
+        public const char Open = '[';
+        public const char Close = ']';
+        public const char Print = '.';
+        public const char Read = ',';
+        public const char Increase = '+';
+        public const char Decrease = '-';
+        public const char MoveLeft = '<';
+        public const char MoveRight = '>';
+    }
+}
