c262871edb0f78407bdb48211656c0dd7dad3a51
Martin Štěpánek <mstepanek015@gmail.com>
2018-11-01 12:20:16 +0100

    Init

---
+++ BrainfuckInterpreter/BrainfuckInterpreter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace BrainfuckInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "../../bf.txt";
+            string content = File.ReadAllText(file);
+            Interpreter i = new Interpreter(content);
+            i.Run();
+            Console.ReadKey();
+        }
+    }
+}
