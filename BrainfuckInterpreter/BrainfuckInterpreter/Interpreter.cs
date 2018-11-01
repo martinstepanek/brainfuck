a0a162421fc2175093f79b9839cc505ee571974b
Martin Štěpánek <mstepanek015@gmail.com>
2018-11-01 13:00:53 +0100

    Add cell operations

--- BrainfuckInterpreter/BrainfuckInterpreter/Interpreter.cs
+++ BrainfuckInterpreter/BrainfuckInterpreter/Interpreter.cs
@@ -1,3 +1,4 @@
+using System;
 
 namespace BrainfuckInterpreter
 {
@@ -5,8 +6,21 @@ namespace BrainfuckInterpreter
     {
         private int[] _memory;
         private int _pointer;
-        private int _currentCell => _memory[_pointer];
-        
+
+        private int _currentCell
+        {
+            get
+            {
+                if (_memory.Length <= _pointer || _pointer < 0)
+                {
+                    throw new MemoryException();
+                }
+
+                return _memory[_pointer];
+            }
+            set { _memory[_pointer] = value; }
+        }
+
         private string _code;
 
         private BlockManager _blockManager;
@@ -21,16 +35,59 @@ namespace BrainfuckInterpreter
         public void Run()
         {
             int codePointer = 0;
+            bool increase;
             while (codePointer < _code.Length)
             {
+                increase = true;
                 char ch = _code[codePointer];
-                if (ch == Chars.Open)
+
+                BasicCellOperations(ch);
+
+                if (ch == Chars.Open && _currentCell == 0)
                 {
-                    
+                    codePointer = _blockManager.GetTo(codePointer);
+                    increase = false;
                 }
-                codePointer++;
+
+                if (ch == Chars.Close && _currentCell != 0)
+                {
+                    codePointer = _blockManager.GetFrom(codePointer);
+                    increase = false;
+                }
+
+                if (ch == Chars.Print)
+                {
+                    Console.Write((char) _currentCell);
+                }
+
+                if (increase)
+                {
+                    codePointer++;
+                }
             }
-        }        
-        
+        }
+
+        private void BasicCellOperations(char ch)
+        {
+            if (ch == Chars.Increase)
+            {
+                _currentCell++;
+            }
+
+            if (ch == Chars.Decrease)
+            {
+                _currentCell--;
+            }
+
+            if (ch == Chars.MoveLeft)
+            {
+                _pointer--;
+            }
+
+            if (ch == Chars.MoveRight)
+            {
+                _pointer++;
+            }
+        }
     }
 }
